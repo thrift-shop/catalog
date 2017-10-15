505845c6dc3c6c8c7fd16e70886e1225f92ccc8c
Nick Nance <nance.nick@gmail.com>
2017-10-14 18:05:36 -0700

    initial poc

---
+++ src/server.ts
@@ -0,0 +1,69 @@
+import * as Hapi from 'hapi'
+
+import { ThriftPlugin } from '@creditkarma/thrift-server-hapi'
+import { CatalogService, IItemArgs, Item } from './codegen/catalog'
+
+const port = 3000
+const server = new Hapi.Server({ debug: { request: [ 'error' ] } })
+
+/**
+ * Register the thrift plugin.
+ *
+ * This will allow us to define Hapi routes for our thrift service(s).
+ * They behave like any other HTTP route handler, so you can mix and match
+ * thrift / REST endpoints on the same server instance.
+ */
+server.register(ThriftPlugin, (err) => {
+    if (err) {
+        throw err
+    }
+})
+
+/**
+ * Implementation of our thrift service.
+ *
+ * Notice the second parameter, "context" - this is the Hapi request object,
+ * passed along to our service by the Hapi thrift plugin. Thus, you have access to
+ * all HTTP request data from within your service implementation.
+ */
+const impl = new CatalogService.Processor({
+    getAll: (context: Hapi.Request): Promise<Item[]> => {
+        const item = new Item({
+            itemId: '1001',
+            version: 1,
+            name: 'Pair of Nike running shoes',
+        })
+        return Promise.resolve([item])
+    },
+})
+
+/**
+ * Route to our thrift service.
+ *
+ * Payload parsing is disabled - the thrift plugin parses the raw request
+ * using whichever protocol is configured (binary, compact, json...)
+ */
+server.route({
+    method: 'POST',
+    path: '/',
+    handler: {
+        thrift: {
+            service: impl,
+        },
+    },
+    config: {
+        payload: {
+            parse: false,
+        },
+    },
+})
+
+/**
+ * Start your hapi server
+ */
+server.start((err) => {
+    if (err) {
+        throw err
+    }
+    server.log('info', `Server running on port ${port}`)
+})
