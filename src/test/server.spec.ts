23e3b89a005a58054fa1929db71eb3b80a440cf1
Kevin Greene <kevin.greene@creditkarma.com>
2018-01-16 18:13:29 -0800

    Update to thrift-server v0.3.6

---
+++ src/test/server.spec.ts
@@ -0,0 +1,45 @@
+import {
+    createClient,
+} from '@creditkarma/thrift-client'
+
+import * as childProcess from 'child_process'
+import { expect } from 'code'
+import * as Lab from 'lab'
+import { CoreOptions } from 'request'
+
+import {
+    CatalogService,
+    Item,
+} from '../codegen/catalog'
+
+export const lab = Lab.script()
+
+const describe = lab.describe
+const it = lab.it
+const before = lab.before
+const after = lab.after
+
+describe('Thrift Server Hapi', () => {
+    let server: any
+    let client: CatalogService.Client<CoreOptions>
+
+    before((done: any) => {
+        server = childProcess.fork('./dist/server.js')
+        client = createClient(CatalogService.Client, {
+            hostName: '0.0.0.0',
+            port: 3010,
+        })
+        setTimeout(done, 1000)
+    })
+
+    it('should return all result for getAll request', async () => {
+        return client.getAll().then((response: Item[]) => {
+            expect(response.length).to.equal(3)
+        })
+    })
+
+    after((done: any) => {
+        server.kill('SIGINT')
+        setTimeout(done, 1000)
+    })
+})
